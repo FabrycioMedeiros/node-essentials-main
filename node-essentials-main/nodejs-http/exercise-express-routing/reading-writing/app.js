42f9190cdfee60457cb396ab006657878dd1c74d
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-09 00:28:03 -0300

    Implementando a capacidade de atualizar dados

--- node-essentials-main/nodejs-http/exercise-express-routing/reading-writing/app.js
+++ node-essentials-main/nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -12,10 +12,28 @@ app.post('/products', function(req, res) {
   products = [ ...products, newProduct]
   res.json(newProduct);
 });
+// Saida: response {"name":"product","id":1}
+//        Closed connection
+// Após nova solicitação "node client-get.js" temos: 
+// Received data [{"name":"product","id":1}]
+// Connection closed
 
-app.put('/products', function (req, res) {
-  // implement
+app.put('/products', function(req, res) {
+  let updatedProduct;
+  products = products.map(p => {
+    if (p.id === req.body.id) {
+      updatedProduct = { ...p, ...req.body };
+      return updatedProduct;
+    }
+    return p;
+  })
+  res.json(updatedProduct);
 });
+// Saida: response {"name":"product-updated","id":1}
+//        Closed connection
+// Após atualização com "node client-get.js" temos:
+// Received data [{"name":"product-updated","id":1}]
+// Connection closed
 
 app.delete('/products/:id', function (req, res) {
   // implement
@@ -24,6 +42,8 @@ app.delete('/products/:id', function (req, res) {
 app.get('/products', (req, res) => {
   res.json(products);
 })
+// Saída: Received data []
+//        Connection closed
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
   
