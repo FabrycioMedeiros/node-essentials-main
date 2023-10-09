f76eccdb7c8e8a492b83190148f064ae3cae6f2c
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-09 00:16:15 -0300

    Implementação de rota e Implementaçao de gravação

--- node-essentials-main/nodejs-http/exercise-express-routing/reading-writing/app.js
+++ node-essentials-main/nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -7,8 +7,10 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.post('/products', function (req, res) {
-  // implement
+app.post('/products', function(req, res) {
+  const newProduct = { ...req.body, id: products.length + 1 }
+  products = [ ...products, newProduct]
+  res.json(newProduct);
 });
 
 app.put('/products', function (req, res) {
@@ -20,7 +22,7 @@ app.delete('/products/:id', function (req, res) {
 });
 
 app.get('/products', (req, res) => {
-  // implement
+  res.json(products);
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
